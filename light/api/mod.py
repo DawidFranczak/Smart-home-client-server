cf46ef4df2c3607c69b4b64d1281e59278be184e
Dawid Franczak <daw.franczak@gmail.com>
2023-04-04 11:23:59 +0200

    added . to import views

--- light/api/mod.py
+++ light/api/mod.py
@@ -23,7 +23,7 @@ def change_light(ip: str, port: int) -> dict:
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(CHANGE_LIGHT, (ip, port))
-        sock.settimeout(1)
+        sock.settimeout(0.5)
         data = sock.recvfrom(128)
         data = data[0].decode("UTF-8")
         sock.close()
